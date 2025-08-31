c3f9c87e242a1b4039273631f3b61009cb390a35
Shivakumar Patil <shivu9887@gmail.com>
2025-08-31 19:30:22 +0530

    removed the unused import causing issue in deployment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header: React.FC = () => {
